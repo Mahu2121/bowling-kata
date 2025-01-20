f83b09f315f20ac097c49c8d52ce4778e2baba76
Pontax02 <ppontanilla07@gmail.com>
2025-01-20 07:31:56 +0100

    test: Modificar test para estar acorde con los puntos de bolos

--- test/test_bowling.py
+++ test/test_bowling.py
@@ -5,24 +5,24 @@ from src.scoreCard import ScoreCard
 
 def test_score_card():
 
-    card = ScoreCard("1234567891")
+    card = ScoreCard("12345678911234567891")
     assert card
 
 
 def test_get_pins():
 
-    PINS = "1234567891"
+    PINS = "12345678911234567891"
     card = ScoreCard(PINS)
-    assert card.get_pins() == PINS 
+    assert card.get_pins() == PINS
 
 
 def test_normal_score():
 
-    PINS = "1234567891"       # PINS == 46
-    card = ScoreCard(PINS)                
-    assert 46 == card.get_score_card_result(PINS)
+    PINS = "12345678911234567891"       # PINS == 92
+    card = ScoreCard(PINS)
+    assert 92 == card.get_score_card_result(PINS)
 
 def test_():
-    PINS = "1234-6-891"      # PINS == 34    
+    PINS = "1234-6-8911234-6-891"      # PINS == 68
     card = ScoreCard(PINS)
-    assert 34 == card.get_score_card_result(PINS)
+    assert 68 == card.get_score_card_result(PINS)
