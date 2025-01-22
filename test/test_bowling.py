ebefd132c5a6a0fa85844902b0224392d2b3d5a2
Hugo Gonzalez Besada <hugogbmahu@gmail.com>
2025-01-22 10:25:37 +0100

    test:

--- test/test_bowling.py
+++ test/test_bowling.py
@@ -18,20 +18,19 @@ def test_get_pins():
 
 def test_normal_score():
 
-    PINS = "12345678911234567891"       # PINS == 92
+    PINS = "12345123451234512345"       # PINS == 92
     card = ScoreCard(PINS)
-    assert 92 == card.get_score()
+    assert 60 == card.get_score()
 
 def test_missed_rolls():
-    PINS = "1234-6-8911234-6-891"      # PINS == 68
+    PINS = "9-9-9-9-9-9-9-9-9-9-"      # PINS == 68
     card = ScoreCard(PINS)
-    assert 68 == card.get_score()
+    assert 90 == card.get_score()
 
 def test_score_spare():
     PINS = "5/5/5/5/5/5/5/5/5/5/5"
     card = ScoreCard(PINS)
     assert 150 == card.get_score()
-
 
 def test_symbols_to_numbers():
     PINS = "5/XX4/1/7/2-23X12"
